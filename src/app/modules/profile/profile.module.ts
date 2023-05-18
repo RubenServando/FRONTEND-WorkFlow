5dda85db7365940316a660d61112ac373a2e7dca
VanessaLaraPerez <vanessa.lara8@gmail.com>
2023-05-18 18:45:21 +0200

    ProfileBackground_Solved

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { BackgroundComponent } from './components/background/background.component';
+
+
+
 
 @NgModule({
-  declarations: [ProfileComponent],
+  declarations: [ProfileComponent,
+    BackgroundComponent],
   imports: [
     CommonModule,
-    ProfileRoutingModule,
-
+    ProfileRoutingModule
   ]
 })
 export class ProfileModule { }
