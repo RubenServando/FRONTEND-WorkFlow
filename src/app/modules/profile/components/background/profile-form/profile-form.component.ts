bb0474e490a2db3e195f34d0a1cac2b79cdc90fd
VanessaLaraPerez <vanessa.lara8@gmail.com>
2023-05-25 19:19:29 +0200

    Updated-profileForm

---
+++ src/app/modules/profile/components/background/profile-form/profile-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './pages/profile.component.html',
+})
+export class ProfileComponent {
+  constructor() {}
+}
