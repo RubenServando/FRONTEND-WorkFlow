5b957a2bc780469f7140fee9244561899b3f34a9
VanessaLaraPerez <vanessa.lara8@gmail.com>
2023-05-25 20:06:38 +0200

    Inprogress

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './pages/profile/profile.component';
-import { BackgroundComponent } from './components/background/background.component';
+import { ProfileFormComponent } from './Pelota/profile-form.component';
+import { BackgroundComponent } from './Pelota/background/background.component';
 
 
 
 
 @NgModule({
   declarations: [ProfileComponent,
+    ProfileFormComponent,
     BackgroundComponent],
   imports: [
     CommonModule,
